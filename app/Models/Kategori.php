080b46c62a0deb341071d92184b8d926df94d07e
iremakalp <iremklp@gmail.com>
2021-05-05 01:04:39 +0300

    E-Ticaret Proje

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kategori extends Model
 {
-    protected $table="kategori";
-    protected $fillable=['kategori_adi'];
+    use SoftDeletes;//veritabanında silinme_tarihi guncellenir
 
+    protected $table="kategori";
+// protected $fillable=['kategori_adi','slug']; //eklenebilir hale getirme
+    protected $guarded=[]; //tum kolonlar eklenebilir hale gelir
+   const CREATED_AT = 'olusturma_tarihi';
+   const UPDATED_AT = 'guncelleme_tarihi';
+   const DELETED_AT='silinme_tarihi';
 }
